36885f571ac8dc4819ec4dcbdbeac99d29fe81cf
Jose Carranco <carranco.jose.r@gmail.com>
2024-05-29 11:49:56 -0500

    make front page fully editable

--- wp-content/themes/pax-law/front-page.php
+++ wp-content/themes/pax-law/front-page.php
@@ -1,16 +1,28 @@
-<?php get_header(); ?>
-
-
-<section id="home-intro" class="d-flex justify-content-center" style="background-image:url('https://carrancomarketing.com/paxlaw/wp-content/uploads/2024/05/peaceful-courthouse-scaled.jpg')">
-<div class="row">
-    <div class="col-12 text-center">
-    <h1>Planning. Protecting.</h1>
-    <h2>Bringing you peace of mind</h2>
-    <a class="btn homeintro-btn" href="#">Real Estate</a>
-    <a class="btn homeintro-btn" href="#">Wills and Trusts</a>
-    <a class="btn homeintro-btn" href="#">Business Law</a>
+<?php
+    get_header();
+    $homepage_intro_photo = get_field('homepage_intro_photo');
+    $homepage_large_text = get_field('homepage_large_text');
+    $homepage_secondary_text = get_field('homepage_secondary_text');
+?>
+<section id="home-intro" class="d-flex justify-content-center" style="background-image:url('<?php echo esc_url($homepage_intro_photo['url']); ?>')">
+    <div class="container-fluid intro-container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1><?php echo $homepage_large_text ?></h1>
+                <h2><?php echo $homepage_secondary_text ?></h2>
+                <div class="row">
+                <?php if( have_rows('homepage_intro_buttons') ): while( have_rows('homepage_intro_buttons') ) : the_row();
+                    $homepage_intro_button_text = get_sub_field('homepage_intro_button_text');
+                    $homepage_button_link = get_sub_field('homepage_button_link');
+                ?>
+                    <div class="col-lg">
+                        <a class="btn homeintro-btn" href="<?php echo esc_url($homepage_button_link) ?>"><?php echo $homepage_intro_button_text?></a>
+                    </div>
+                <?php endwhile; else : endif; ?>
+                </div>    
+            </div>
+        </div>
     </div>
-</div>
 </section>
 
 <section class="container">
